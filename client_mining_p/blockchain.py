966e7016c692bee1b779c41d63c3e8099fd24001
ogwurujohnson <ogwurujohnson@gmail.com>
2019-10-30 16:51:53 +0100

    coment out proof_of_work mehtod from class

--- client_mining_p/blockchain.py
+++ client_mining_p/blockchain.py
@@ -92,23 +92,23 @@ class Blockchain(object):
     def last_block(self):
         return self.chain[-1]
 
-    def proof_of_work(self, block):
-        """
-        Simple Proof of Work Algorithm
-        Stringify the block and look for a proof.
-        Loop through possibilities, checking each one against `valid_proof`
-        in an effort to find a number that is a valid proof
-        :return: A valid proof for the provided block
-        """
+    # def proof_of_work(self, block):
+    #     """
+    #     Simple Proof of Work Algorithm
+    #     Stringify the block and look for a proof.
+    #     Loop through possibilities, checking each one against `valid_proof`
+    #     in an effort to find a number that is a valid proof
+    #     :return: A valid proof for the provided block
+    #     """
         
-        # return proof
-        block_string = json.dumps(block, sort_keys=True).encode()
+    #     # return proof
+    #     block_string = json.dumps(block, sort_keys=True).encode()
 
-        proof = 0
-        while self.valid_proof(block_string, proof) is False:
-            proof += 1
+    #     proof = 0
+    #     while self.valid_proof(block_string, proof) is False:
+    #         proof += 1
         
-        return proof
+    #     return proof
 
 
     @staticmethod
